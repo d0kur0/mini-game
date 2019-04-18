12c71985d21705fecc5669bf094d52a88928f17b
d0kur0 <dev.dokuro@gmail.com>
2019-04-18 12:06:12 +0300

    Добавил процесс рендеринга поля, точка входа остальных частей

---
+++ js/game/renderLoop.js
@@ -0,0 +1,21 @@
+import playerControls from "./playerControls.js"
+import state from "./state.js";
+
+export default function renderLoop () {
+    const gameFieldRect = document.querySelector('.game__field')
+        .getBoundingClientRect();
+
+    state.fieldRect = {
+        x: gameFieldRect.x,
+        y: gameFieldRect.y,
+
+        height: gameFieldRect.height,
+        width: gameFieldRect.width,
+
+        xEnd: gameFieldRect.x + gameFieldRect.width,
+        yEnd: gameFieldRect.y + gameFieldRect.height
+    };
+
+    playerControls();
+    console.log('render loop');
+};
