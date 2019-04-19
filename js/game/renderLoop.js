29fba545c8499eb6ccb1cc5d41a1cbe003d3e656
d0kur0 <31435565+d0kur0@users.noreply.github.com>
2019-04-20 00:03:58 +0300

    Добавил ковычки

--- js/game/renderLoop.js
+++ js/game/renderLoop.js
@@ -2,19 +2,9 @@ import playerControls from "./playerControls.js"
 import state from "./state.js";
 
 export default function renderLoop () {
-    const gameFieldRect = document.querySelector('.game__field')
-        .getBoundingClientRect();
+    const field = document.querySelector('.game-field');
 
-    state.fieldRect = {
-        x: gameFieldRect.x,
-        y: gameFieldRect.y,
-
-        height: gameFieldRect.height,
-        width: gameFieldRect.width,
-
-        xEnd: gameFieldRect.x + gameFieldRect.width,
-        yEnd: gameFieldRect.y + gameFieldRect.height
-    };
+    state.fieldRect = field.getBoundingClientRect();
 
     playerControls();
 };
