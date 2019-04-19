d050f8cc26a5b68eab2be3d2c205f1fc349ae772
d0kur0 <31435565+d0kur0@users.noreply.github.com>
2019-04-20 00:03:43 +0300

    Переписал логику, возложил движение на внешний класс событий

--- js/game/playerControls.js
+++ js/game/playerControls.js
@@ -1,83 +1,39 @@
 import state from "./state.js";
+import controlEvents from "./controlEvents.js";
 
 export default function playerControls () {
 
+    const events = new controlEvents();
+
     window.addEventListener('keydown', (event) => {
-        const player = document.querySelector('.game__player');
-        const playerRect = player.getBoundingClientRect();
-        const stepSize = 10;
-
-        let currentX = player.style.left.match(/\d+/g);
-        let currentY = player.style.top.match(/\d+/g);
-
-        let currentPosition = {
-            x: parseInt(currentX === null ? 0 : currentX),
-            y: parseInt(currentY === null ? 0 : currentY)
-        };
-
-        let newPosition = {
-            x: currentPosition.x,
-            y: currentPosition.y
-        };
-
-        console.log(newPosition);
+        state.player = document.querySelector('.game-player');
+        state.playerRect = state.player.getBoundingClientRect();
 
         switch (event.which) {
             // left
             case 37:
-                if (!currentPosition.x) {
-                    return;
-                }
-
-                newPosition.x -= stepSize;
+                events.moveLeft();
                 break;
 
             // right
             case 39:
-                if ((newPosition.x + playerRect.width) + stepSize >= state.fieldRect.width) {
-                    let outside = state.fieldRect.width - (newPosition.x + stepSize);
-
-                    if (outside > 0) {
-                        newPosition.x = state.fieldRect.width - playerRect.width;
-                    } else {
-                        return;
-                    }
-                }
-                else
-                {
-                    newPosition.x += stepSize;
-                }
+                events.moveRight();
                 break;
 
             // top
             case 38:
-                if (!currentPosition.y) {
-                    return;
-                }
-
-                newPosition.y -= stepSize;
+                events.jump();
                 break;
 
             // bottom
             case 40:
-                if ((newPosition.y + playerRect.height) + stepSize >= state.fieldRect.height) {
-                    let outside = state.fieldRect.height - (newPosition.y + stepSize);
-
-                    if (outside > 0) {
-                        newPosition.y = state.fieldRect.height - playerRect.height;
-                    } else {
-                        return;
-                    }
-                }
-                else
-                {
-                    newPosition.y += stepSize;
-                }
+                // ...
                 break;
         }
-
-        player.style.left = newPosition.x + 'px';
-        player.style.top  = newPosition.y + 'px';
     });
+
+    window.addEventListener('keyup', () => {
+        events.stop();
+    })
 
 }
