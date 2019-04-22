9272848f5f461bc4bccec66433f2da5c39c784b1
d0kur0 <dev.dokuro@gmail.com>
2019-04-22 15:19:45 +0300

    Переписал систему движения персонажа

--- js/game/playerControls.js
+++ js/game/playerControls.js
@@ -3,37 +3,29 @@ import controlEvents from "./controlEvents.js";
 
 export default function playerControls () {
 
-    const events = new controlEvents();
+  const events = new controlEvents();
 
-    window.addEventListener('keydown', (event) => {
-        state.player = document.querySelector('.game-player');
-        state.playerRect = state.player.getBoundingClientRect();
+  window.addEventListener('keydown', event => {
 
-        switch (event.which) {
-            // left
-            case 37:
-                events.moveLeft();
-                break;
+    switch (event.which) {
+      // left
+      case 37:
+        events.moveStart('left');
+        break;
+      case 39:
+        events.moveStart('right');
+        break;
+      case 38:
+        // ...
+        break
+      case 40:
+        // ...
+        break;
+    }
+  });
 
-            // right
-            case 39:
-                events.moveRight();
-                break;
-
-            // top
-            case 38:
-                events.jump();
-                break;
-
-            // bottom
-            case 40:
-                // ...
-                break;
-        }
-    });
-
-    window.addEventListener('keyup', () => {
-        events.stop();
-    })
+  window.addEventListener('keyup', () => {
+    events.moveStop();
+  });
 
 }
