9382f5d3fe5296cef91cb05e3ae0ad1b8f95a5f0
d0kur0 <dev.dokuro@gmail.com>
2019-04-22 15:20:39 +0300

    Изменил получение DOM элементов нужных для работы

--- js/game/renderLoop.js
+++ js/game/renderLoop.js
@@ -2,9 +2,10 @@ import playerControls from "./playerControls.js"
 import state from "./state.js";
 
 export default function renderLoop () {
-    const field = document.querySelector('.game-field');
 
-    state.fieldRect = field.getBoundingClientRect();
+    state.field = document.querySelector('.game-field');
+    state.fieldRect = state.field.getClientRects();
+    state.player = document.querySelector('.game-player');
 
     playerControls();
 };
