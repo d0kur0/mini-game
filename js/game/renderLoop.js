2dcdf49400bcdb6ae4fce1a724ebec2ee51f126b
d0kur0 <dev.dokuro@gmail.com>
2019-04-22 15:30:13 +0300

    Убрал получение Rect информации об игровом поле, заменил на offsetWidth от самого элемента

--- js/game/renderLoop.js
+++ js/game/renderLoop.js
@@ -4,7 +4,6 @@ import state from "./state.js";
 export default function renderLoop () {
 
     state.field = document.querySelector('.game-field');
-    state.fieldRect = state.field.getClientRects();
     state.player = document.querySelector('.game-player');
 
     playerControls();
